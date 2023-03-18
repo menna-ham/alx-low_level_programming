10d309ee72475bb0106572ebff3784e8f71bddb9
menna-ham <menna.hamouda58@gmail.com>
2023-03-18 06:34:33 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-int main (void)
+/**
+ * main - starting point of the program
+ *
+ * Return: 0
+ */
+int main(void)
 {
-	printf('with proper grammar, but the outcome is a piece of art,\n');
-	return 0;
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
