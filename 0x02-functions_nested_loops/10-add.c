94ec15e34037ba49afb5926e08b624613bf1cd4f
menna-ham <menna.hamouda58@gmail.com>
2023-03-21 16:49:02 -0700

    loop

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+/**
+* add - adds two integers and returns the result
+* @n1: the first integer for the calculation
+* @n2: the second integer for the calculation
+*
+* Return: the sum of @n1 and @n2
+*/
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
