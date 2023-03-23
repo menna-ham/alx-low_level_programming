0570b4b2a7e9e47f331e200d9e04b865501195a1
menna-ham <menna.hamouda58@gmail.com>
2023-03-23 06:48:05 -0700

    nested

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - starting point of the program
+*
+* Return: Always 0
+*/
+int main(void)
+{
+	long i;
+	long num = 612852475143;
+
+	for (i = 2; i < num; i++)
+	{
+		if (num % i == 0)
+			num /= i;
+	}
+	printf("%li\n", num);
+	return (0);
+}
