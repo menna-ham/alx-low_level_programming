d6862adabe6c088a993a1fa4d6321e2f27c96305
menna-ham <menna.hamouda58@gmail.com>
2023-04-11 13:20:40 -0700

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - starting point of the program
+* @argc: argument count
+* @argv: argument list
+*
+* Return: Always 0
+*/
+int main(int argc, __attribute__((unused)) char **argv)
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
