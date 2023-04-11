2c864ed76d33e3ed8a40dce5e1c2f8bed0a788c9
menna-ham <menna.hamouda58@gmail.com>
2023-04-11 13:17:12 -0700

    argc

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - starting point of the program
+* @argc: argument count
+* @argv: argument list
+*
+* Return: Always 0
+*/
+int main(__attribute__((unused)) int argc, char **argv)
+{
+	printf("%s\n", *argv);
+
+	return (0);
+}
