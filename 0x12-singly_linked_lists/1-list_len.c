98bc2f8b4d22904816fc759a96d96b7aeb4b1ed8
menna-ham <menna.hamouda58@gmail.com>
2023-04-28 03:39:44 -0700

    list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+* list_len - returns the number of elements in a linked list_t list
+* @h: pointer to the head of the list
+*
+* Return: number of elements in list_t list
+*/
+size_t list_len(const list_t *h)
+{
+	size_t n_count = 0;
+
+	while (h->next != NULL)
+	{
+		h = h->next;
+		n_count++;
+	}
+
+	n_count++;
+
+	return (n_count);
+}
